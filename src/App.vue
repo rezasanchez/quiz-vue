f91a0acbd056f87bd4da58916a41a551bd7b0738
Reza sanchez <chairizamuttaqin1603@gmail.com>
2024-11-23 15:04:10 +0700

    Membuat App Quiz

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref, watch } from "vue";
+import srcQuiz from "./data/quizes.json";
+
+import QuizCard from "./components/QuizCard.vue";
+
+const quizes = ref(srcQuiz);
+const search = ref("");
+
+watch(search, () => {
+    quizes.value = srcQuiz.filter((quiz) => {
+        return quiz.title
+            .toLowerCase()
+            .includes(search.value.toLocaleLowerCase());
+    });
+});
+</script>
+
+<template>
+    <main>
+        <header>
+            <h1 id="title">QuizVue</h1>
+            <input
+                v-model.trim="search"
+                type="text"
+                placeholder="Search"
+                id="search-input"
+            />
+        </header>
+        <section id="quiz-container">
+            <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
+                <img :src="quiz.img" :alt="quiz.title" />
+                <div class="card-body">
+                    <h2>{{ quiz.title }}</h2>
+                    <p>{{ quiz.questions.length }} Questions</p>
+                </div>
+            </div> -->
+            <QuizCard v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+        </section>
+    </main>
+</template>
+
+<style scoped>
+main {
+    max-width: 900px;
+    margin: 0 auto;
+}
+
+header {
+    margin-top: 30px;
+    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+}
+
+#title {
+    font-weight: bold;
+    margin-right: 30px;
+}
+
+#search-input {
+    border: none;
+    background-color: #c9c9c9d8;
+    padding: 10px;
+    border-radius: 8px;
+}
+
+#quiz-container {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 20px;
+}
+</style>
